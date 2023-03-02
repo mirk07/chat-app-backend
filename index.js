b1f0e140290aa985b9c6adf34b1f08890df9c688
mirk07 <93538327+mirk07@users.noreply.github.com>
2023-03-02 22:40:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const server = app.listen(process.env.PORT, () =>
 
 const io = socket(server, {
   cors: {
-    origin: "*",
+    origin: ["*", "https://chat-app-mirko.onrender.com"],
     credentials: "true",
   },
 });
